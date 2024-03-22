b7bd0301419301b45849c30f4a2abe99c0ca5a2f
CharlesLayet <cogo.carlos.a@gmail.com>
2024-03-21 20:14:39 -0600

    finishing the categories section

--- src/scripts/components/categorires.jsx
+++ src/scripts/components/categorires.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import brocoli from '../../assets/fruits/brocoli.png';
+import cebolla from '../../assets/fruits/cebolla.png';
+import hongos from '../../assets/fruits/hongos.png';
+import lechuga from '../../assets/fruits/lechuga.png';
+import mangos from '../../assets/fruits/mangos.png';
+import naranja from '../../assets/fruits/naranja.png';
+import shapSmall from '../../assets/fruits/shap-small.png';
+
 const Categorires = () => {
     return (
         <section className='categories'>
@@ -17,9 +25,222 @@ const Categorires = () => {
                 <span>Hogar</span>
                 <span>Cremeria</span>
             </div>
-            <div className='categories__cards u-mt-large'>
-                <div className='categories__cards-card'>
-
+            <div className='fruits'>
+                <div className='categories__cards u-mt-large'>
+                    <div className='categories__cards-card'>
+                        <div className='categories__cards-card-extras'>
+                            <span className="material-symbols-outlined">
+                                favorite
+                            </span>
+                            <span className="material-symbols-outlined">
+                                shopping_cart_checkout
+                            </span>
+                            <span className="material-symbols-outlined">
+                                open_in_full
+                            </span>
+                        </div>
+                        <div className='categories__cards-card-imageContainer'>
+                            <img
+                                alt={ brocoli }
+                                className='categories__cards-card-image'
+                                src={ brocoli }
+                            />
+                            <img
+                                alt='fruits'
+                                className='categories__cards-card-imageSeparator'
+                                src={ shapSmall }
+                            />
+                        </div>
+                        <p className='categories__cards-card-name'>Brocolí</p>
+                        <div className='categories__cards-card-prices'>
+                            <span className='categories__cards-card-price'>
+                                $50.00
+                            </span>
+                            <span className='categories__cards-card-subPrice'>
+                                $65.00
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className='categories__cards u-mt-large'>
+                    <div className='categories__cards-card'>
+                        <div className='categories__cards-card-extras'>
+                            <span className="material-symbols-outlined">
+                                favorite
+                            </span>
+                            <span className="material-symbols-outlined">
+                                shopping_cart_checkout
+                            </span>
+                            <span className="material-symbols-outlined">
+                                open_in_full
+                            </span>
+                        </div>
+                        <div className='categories__cards-card-imageContainer'>
+                            <img
+                                alt={ cebolla }
+                                className='categories__cards-card-image'
+                                src={ cebolla }
+                            />
+                            <img
+                                alt='fruits'
+                                className='categories__cards-card-imageSeparator'
+                                src={ shapSmall }
+                            />
+                        </div>
+                        <p className='categories__cards-card-name'>Cebolla</p>
+                        <div className='categories__cards-card-prices'>
+                            <span className='categories__cards-card-price'>
+                                $50.00
+                            </span>
+                            <span className='categories__cards-card-subPrice'>
+                                $65.00
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className='categories__cards u-mt-large'>
+                    <div className='categories__cards-card'>
+                        <div className='categories__cards-card-extras'>
+                            <span className="material-symbols-outlined">
+                                favorite
+                            </span>
+                            <span className="material-symbols-outlined">
+                                shopping_cart_checkout
+                            </span>
+                            <span className="material-symbols-outlined">
+                                open_in_full
+                            </span>
+                        </div>
+                        <div className='categories__cards-card-imageContainer'>
+                            <img
+                                alt={ hongos }
+                                className='categories__cards-card-image'
+                                src={ hongos }
+                            />
+                            <img
+                                alt='fruits'
+                                className='categories__cards-card-imageSeparator'
+                                src={ shapSmall }
+                            />
+                        </div>
+                        <p className='categories__cards-card-name'>Hongos</p>
+                        <div className='categories__cards-card-prices'>
+                            <span className='categories__cards-card-price'>
+                                $50.00
+                            </span>
+                            <span className='categories__cards-card-subPrice'>
+                                $65.00
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className='categories__cards u-mt-large'>
+                    <div className='categories__cards-card'>
+                        <div className='categories__cards-card-extras'>
+                            <span className="material-symbols-outlined">
+                                favorite
+                            </span>
+                            <span className="material-symbols-outlined">
+                                shopping_cart_checkout
+                            </span>
+                            <span className="material-symbols-outlined">
+                                open_in_full
+                            </span>
+                        </div>
+                        <div className='categories__cards-card-imageContainer'>
+                            <img
+                                alt={ lechuga }
+                                className='categories__cards-card-image'
+                                src={ lechuga }
+                            />
+                            <img
+                                alt='fruits'
+                                className='categories__cards-card-imageSeparator'
+                                src={ shapSmall }
+                            />
+                        </div>
+                        <p className='categories__cards-card-name'>Lechuga</p>
+                        <div className='categories__cards-card-prices'>
+                            <span className='categories__cards-card-price'>
+                                $50.00
+                            </span>
+                            <span className='categories__cards-card-subPrice'>
+                                $65.00
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className='categories__cards u-mt-large'>
+                    <div className='categories__cards-card'>
+                        <div className='categories__cards-card-extras'>
+                            <span className="material-symbols-outlined">
+                                favorite
+                            </span>
+                            <span className="material-symbols-outlined">
+                                shopping_cart_checkout
+                            </span>
+                            <span className="material-symbols-outlined">
+                                open_in_full
+                            </span>
+                        </div>
+                        <div className='categories__cards-card-imageContainer'>
+                            <img
+                                alt={ mangos }
+                                className='categories__cards-card-image'
+                                src={ mangos }
+                            />
+                            <img
+                                alt='fruits'
+                                className='categories__cards-card-imageSeparator'
+                                src={ shapSmall }
+                            />
+                        </div>
+                        <p className='categories__cards-card-name'>Mangos</p>
+                        <div className='categories__cards-card-prices'>
+                            <span className='categories__cards-card-price'>
+                                $50.00
+                            </span>
+                            <span className='categories__cards-card-subPrice'>
+                                $65.00
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className='categories__cards u-mt-large'>
+                    <div className='categories__cards-card'>
+                        <div className='categories__cards-card-extras'>
+                            <span className="material-symbols-outlined">
+                                favorite
+                            </span>
+                            <span className="material-symbols-outlined">
+                                shopping_cart_checkout
+                            </span>
+                            <span className="material-symbols-outlined">
+                                open_in_full
+                            </span>
+                        </div>
+                        <div className='categories__cards-card-imageContainer'>
+                            <img
+                                alt={ naranja }
+                                className='categories__cards-card-image'
+                                src={ naranja }
+                            />
+                            <img
+                                alt='fruits'
+                                className='categories__cards-card-imageSeparator'
+                                src={ shapSmall }
+                            />
+                        </div>
+                        <p className='categories__cards-card-name'>Naranja</p>
+                        <div className='categories__cards-card-prices'>
+                            <span className='categories__cards-card-price'>
+                                $50.00
+                            </span>
+                            <span className='categories__cards-card-subPrice'>
+                                $65.00
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
 
